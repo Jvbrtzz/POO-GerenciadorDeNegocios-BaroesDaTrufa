2653f0a0a6c3a9a560c706e1025a40e7e0231b4a
João Victor Bortoluzzi <66214842+Jvbrtzz@users.noreply.github.com>
2021-08-11 19:39:42 -0300

    Update GerenciadorDeNegocios.java

--- GerenciadorDeNegocios/src/gerenciadordenegocios/GerenciadorDeNegocios.java
+++ GerenciadorDeNegocios/src/gerenciadordenegocios/GerenciadorDeNegocios.java
@@ -15,7 +15,7 @@ public class GerenciadorDeNegocios {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Olá mundo");
     }
     
 }
