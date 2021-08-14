96de50c45e1198b30d2533d912936b72ecd16e04
gabrielripper1 <gabrielripper@id.uff.br>
2021-08-14 13:02:34 -0300

    Update Servico.java

--- GerenciadorDeNegocios/src/Modelos/Servico.java
+++ GerenciadorDeNegocios/src/Modelos/Servico.java
@@ -10,5 +10,29 @@ package Modelos;
  * @author jvbor
  */
 public class Servico {
-    
+    private int id;
+    private string descricao;
+    private float valor;
+
+    public Servico(int id, String descricao, float valor) {
+        this.id = id;
+        this.descricao = descricao;
+        this.valor = valor;
+    }
+
+    public String getDescricao(){
+        return descricao;
+    }
+
+    public void setDescricao(String descricao){
+        this.descricao = descricao;
+    }
+
+    public float getValor(){
+        return valor;
+    }
+
+    public float setValor(float valor){
+        this.valor = valor;
+    }
 }
