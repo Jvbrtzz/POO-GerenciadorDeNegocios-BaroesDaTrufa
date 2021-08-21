0a2fe4a64fa0b639abeef294d71c0f325534a7e9
gabrielripper1 <gabrielripper@id.uff.br>
2021-08-21 13:35:53 -0300

    fix controle
    fix controle

--- GerenciadorDeNegocios/src/Estoque/controle_de_estoque.java
+++ GerenciadorDeNegocios/src/Estoque/controle_de_estoque.java
@@ -7,7 +7,7 @@ package Estoque;
 
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
-import java.io.FileReader;  
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -22,27 +22,27 @@ public class controle_de_estoque {
             FileReader arq = new FileReader(path);
             BufferedReader lerArq = new BufferedReader(arq);
             String linha="";
-            try{
+            try {
                 linha = lerArq.readLine();
                 while(linha!=null){
-                    conteudo += linha;
+                    conteudo += linha+"\n";
                     linha = lerArq.readLine();
                 }
                 arq.close();
-            }   catch (IOException ex){
-                    conteudo = "Ops! Arquivo não pôde ser lido.";
-            }
-            if(conteudo.contains("Ops!"))
-                return "";
-            else{
                 return conteudo;
+            } catch (IOException ex) {
+                System.out.println("Opa! Não foi possível ler o arquivo!");
+                return "";
             }
+        } catch (FileNotFoundException ex) {
+            System.out.println("Opa! O arquivo não foi encontrado!");
+            return "";
         }
     }
     
-    public tatic boolean Write(String path, String info){
-        try{
-            FileWriter arq = new FileWriter(path);
+    public static boolean Write(String Caminho,String Texto){
+        try {
+            FileWriter arq = new FileWriter(Caminho);
             PrintWriter gravarArq = new PrintWriter(arq);
             gravarArq.println(Texto);
             gravarArq.close();
