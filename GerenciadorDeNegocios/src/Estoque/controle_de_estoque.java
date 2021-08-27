2eb2703abd0f97407a6759a2da107a8921475322
gabrielripper1 <gabrielripper@id.uff.br>
2021-08-27 15:38:49 -0300

    att estoque

--- GerenciadorDeNegocios/src/Estoque/controle_de_estoque.java
+++ GerenciadorDeNegocios/src/Estoque/controle_de_estoque.java
@@ -5,51 +5,15 @@
  */
 package Estoque;
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.PrintWriter;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 /**
  *
  * @author oi
  */
-public class controle_de_estoque {
-    public static String Read(String path){
-        String conteudo = "";
-        try {
-            FileReader arq = new FileReader(path);
-            BufferedReader lerArq = new BufferedReader(arq);
-            String linha="";
-            try {
-                linha = lerArq.readLine();
-                while(linha!=null){
-                    conteudo += linha+"\n";
-                    linha = lerArq.readLine();
-                }
-                arq.close();
-                return conteudo;
-            } catch (IOException ex) {
-                System.out.println("Opa! Não foi possível ler o arquivo!");
-                return "";
-            }
-        } catch (FileNotFoundException ex) {
-            System.out.println("Opa! O arquivo não foi encontrado!");
-            return "";
-        }
-    }
-    
-    public static boolean Write(String Caminho,String Texto){
-        try {
-            FileWriter arq = new FileWriter(Caminho);
-            PrintWriter gravarArq = new PrintWriter(arq);
-            gravarArq.println(Texto);
-            gravarArq.close();
-            return true;
-        }catch(IOException e){
-            System.out.println(e.getMessage());
-            return false;
-        }
+public class controle_de_estoque {  
+    public static leitor_csv(controle_de_estoque x) {
+        Files.lines(Paths.get("estoque.csv"))
+        .forEach(System.out::println);        
     }
 }
